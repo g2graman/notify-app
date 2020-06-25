32f124886535434d0f7fd4d4797bcf2dce152945
Francesco <fran.gramano@mail.utoronto.ca>
2020-06-24 22:43:52 -0400

    Seems to work!

--- src/functions/page.js
+++ src/functions/page.js
@@ -1,8 +1,31 @@
-exports.handler = async (event, context, callback) => {
-  return callback(null, {
+const pdClient = require('node-pagerduty');
+
+const {
+  PAGERDUTY_TOKEN,
+  PAGERDUTY_FROM,
+  PAGERDUTY_SERVICE_ID
+} = process.env
+
+const pager = new pdClient(PAGERDUTY_TOKEN);
+
+exports.handler = async (event, context) => {
+  const { incidents } = pager;
+
+  const newIncident = await incidents.createIncident(PAGERDUTY_FROM,{
+    "incident": {
+      "type": "incident",
+      "title": "FREAK OUT",
+      "service": {
+        "id": PAGERDUTY_SERVICE_ID,
+        "type": "service_reference"
+      }
+    }
+  });
+
+  return {
     statusCode: 200,
     body: JSON.stringify({
-      message: "it works",
-    }),
-  });
-};
+      data: 'It worked!'
+    })
+  };
+}
